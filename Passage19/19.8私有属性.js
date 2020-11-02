a264952894effe73eb3b31a352804c9494b5f823
cat9yuko <1309252868@qq.com>
2020-11-02 13:54:34 +0800

    新增

--- Passage19/19.8私有属性.js
+++ Passage19/19.8私有属性.js
@@ -1,6 +1,7 @@
 /*
  * @Author: Cat9Yuko 
  * @Date: 2020-11-02 11:44:09 
- * @Last Modified by:   Cat9Yuko 
- * @Last Modified time: 2020-11-02 11:44:09 
+ * @Last Modified by: Cat9Yuko
+ * @Last Modified time: 2020-11-02 12:47:00
  */
+
